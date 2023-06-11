d960beb671bb4a0bc1342313c389f832ed19a005
Sakshi Shinde <sakshishinde1749@gmail.com>
2023-06-12 00:31:51 +0530

    used product.js file to create array of data

--- Projects/javascript-amazon-project/data/products.js
+++ Projects/javascript-amazon-project/data/products.js
@@ -1,3 +1,4 @@
+//saving the data
 const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
--- Projects/javascript-amazon-project/scripts/amzon.js
+++ Projects/javascript-amazon-project/scripts/amzon.js
@@ -1,36 +1,7 @@
-//save the data
-const products = [
-    {
-        image : `images/products/athletic-cotton-socks-6-pairs.jpg`,
-        name : `Black and Gray Athletic Cotton Socks - 6 Pairs`,
-        rating : {
-            stars : 4.5,
-            count : 87
-        },
-        priceCents : 1090
-    },
-    {
-        image : 'images/products/intermediate-composite-basketball.jpg',
-        name : 'Intermediate Size Basketball',
-        rating : {
-            stars : 4,
-            count : 127
-        },
-        priceCents : 2095
-    },
-    {
-        image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating : {
-            stars : 4.5,
-            count : 56
-        },
-        priceCents : 799
-    }
-];
 
+//generate html code
 let productHTML = '';
-products.forEach(function (products){
+products.forEach(function generateHTML(products){
     productHTML += 
    `<div class="product-container">
         <div class="product-image-container">
@@ -82,5 +53,7 @@ products.forEach(function (products){
     </div>`
 })
 
+
+//select element and put your html code into element
 document.querySelector('.js-product-grid')
     .innerHTML = productHTML;
